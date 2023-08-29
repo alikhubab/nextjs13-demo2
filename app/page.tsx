7e40689db6f5cd65a50b326859d4e9e4e72f3dcd
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-08-29 21:38:26 +0500

    Add server side action

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,42 @@ export default async function Home() {
 
   const products: Product[] = await res.json();
 
+  const addProductToDatabase = async (e: FormData) => {
+    "use server";
+
+    const product = e.get("product")?.toString();
+    const price = e.get("price")?.toString();
+
+    if (!product || !price) return;
+    const newProduct: Product = {
+      product,
+      price,
+    };
+
+    await fetch("https://64ee17df1f87218271424cfe.mockapi.io/products", {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <main>
       <h1 className="text-3xl font-bold text-center">Product Warehouse</h1>
-      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+      <form
+        action={addProductToDatabase}
+        className="flex flex-col gap-5 max-w-xl mx-auto p-5"
+      >
         <input
+          name="product"
           placeholder="Enter product name..."
           type="text"
           className="border border-gray-300  p-2 rounded-md"
         />
         <input
+          name="price"
           type="text"
           placeholder="Enter Price.."
           className="border border-gray-300  p-2 rounded-md"
